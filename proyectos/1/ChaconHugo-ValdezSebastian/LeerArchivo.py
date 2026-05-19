b10202455e2af5151340ebff710b35972e410dea
Sebastian <bastianvaldez@outlook.com>
2026-05-18 20:21:10 -0600

    agrego la funcion listar_directorio

--- proyectos/1/ChaconHugo-ValdezSebastian/LeerArchivo.py
+++ proyectos/1/ChaconHugo-ValdezSebastian/LeerArchivo.py
@@ -33,6 +33,7 @@ def analizar_superbloque():
         print(f"14-18 ->  Version: {version}")
         if version != "26-2":
             print("[ERROR] No es la version esperada")
+            print("Pero se continua porque asi dijo el profe")
             #return    #QUITAR EL RETURN COMENTADO---------------------------------------------------------------------------- 
             #Creo que el profe lo subio mal porque en el github lo modifico 2 dias antes de corregir el readme
             #y el readme antes estaba mal
@@ -56,7 +57,65 @@ def analizar_superbloque():
 
 
 
+def listar_directorio():
+    ENTRY_SIZE = 64
+    START_OFFSET = 1 * CLUSTER_SIZE  #empieza en 1 porque superbloque es 0
+    DIR_SIZE = 8 * CLUSTER_SIZE      #directorio tiene 8 clusters
+    if not os.path.exists(IMG_PATH):
+        print(f"[ERROR] No se encontró el archivo en: {IMG_PATH}")
+        return
+
+    with open(IMG_PATH, "rb") as f:
+        f.seek(START_OFFSET)
+        directorio_bytes = f.read(DIR_SIZE)
+        
+        # Cabecera 
+        print(f"\n{'Nombre Archivo':<16} | {'Tamaño':<10} | {'Cluster Inicial':<12} | {'Creación':<20} | {'Modificación':<20}")
+        print("-" * 95)
+        
+        for i in range(0, DIR_SIZE, ENTRY_SIZE):
+            entrada = directorio_bytes[i : i + ENTRY_SIZE]
+          
+            if entrada[0:1] == b'/': #CON ESTE IF YA NO MUESTRA LOS VACIOS-----------------------------------------------------------------------
+               continue
+            
+            # 1-16 nombre archivo
+            bytes_nombre = entrada[1:16].split(b'\x00')[0]
+            nombre = bytes_nombre.decode('ascii').strip()
+                            
+            # 16-20 tamaño archivo
+            tamano = struct.unpack('<I', entrada[16:20])[0]
+            
+            # 4. 20-23 cluster inicial
+            cluster_inicial = struct.unpack('<I', entrada[20:24])[0]
+            
+            # 5.30-43 fecha y hora de creacion
+            bytes_creacion = entrada[30:44].split(b'\x00')[0]
+            fecha_creacion_raw = bytes_creacion.decode('ascii').strip()
+            fecha_creacion = formatear_fecha(fecha_creacion_raw)
+            
+            # 6. 50-63 fecha y hora de ultima modificacion
+            bytes_modificacion = entrada[50:64].split(b'\x00')[0]
+            fecha_mod_raw = bytes_modificacion.decode('ascii').strip()
+            fecha_mod = formatear_fecha(fecha_mod_raw)
+            
+            # imprimir fila 
+            print(f"{nombre:<16} | {tamano:<10} | {cluster_inicial:<15} | {fecha_creacion:<20} | {fecha_mod:<20}")
+
+def formatear_fecha(cadena_fecha):
+    """Convierte '20260108182600' en '2026-01-08 18:26:00' para que sea legible."""
+    if len(cadena_fecha) < 14:
+        return "Fecha inválida"
+    anio = cadena_fecha[0:4]
+    mes = cadena_fecha[4:6]
+    dia = cadena_fecha[6:8]
+    hora = cadena_fecha[8:10]
+    minuto = cadena_fecha[10:12]
+    segundo = cadena_fecha[12:14]
+    return f"{anio}-{mes}-{dia} {hora}:{minuto}:{segundo}"
+
 
 
 if __name__ == "__main__":
     analizar_superbloque()
+    listar_directorio()
